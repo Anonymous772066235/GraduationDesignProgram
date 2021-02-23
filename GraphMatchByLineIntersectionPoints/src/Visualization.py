afbeb990965da1db4a6bd9153e906c1fdb4b3b42
WooChi <58173016+Anonymous772066235@users.noreply.github.com>
2021-02-23 18:25:01 +0800

    Add files via upload

---
+++ GraphMatchByLineIntersectionPoints/src/Visualization.py
@@ -0,0 +1,46 @@
+# File      :Visualization.py
+# Author    :WJ
+# Function  :
+# Time      :2021/01/14
+# Version   :
+# Amend     :
+
+
+import matplotlib.pyplot as plt
+
+
+def VisualizeMacthLine(P_dlg, P_dopp, row_ind, col_ind):
+    for i in range(len(row_ind)):
+        X = [P_dlg[row_ind, 0], P_dopp[col_ind, 0]]
+        Y = [P_dlg[row_ind, 1], P_dopp[col_ind, 1]]
+        plt.plot(X, Y, color='b')
+
+def VisualizePoints(points, color, label):
+    plt.scatter(points[:, 0], points[:, 1], color=color, label=label)
+
+def Visualize2PointClouds(cloud1,cloud2, pic='pic',feature1=['blue','cloud1','.'],feature2=['red','cloud2','.']):
+    plt.figure(figsize=(16, 9))
+    plt.axis('equal')
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    plt.scatter(cloud1[:, 0], cloud1[:, 1], c=feature1[0],label=feature1[1] ,marker=feature1[2])
+    plt.scatter(cloud2[:, 0], cloud2[:, 1], c=feature2[0],label=feature2[1] ,marker=feature2[2])
+    plt.title(pic)
+    plt.legend(loc='best')
+    plt.savefig('..\\pic\\' + pic + '.png')
+    plt.close()
+
+def VisualizeMatch(P_dopp, P_dlg, row, col, pic='凸多边形顶点'):
+    plt.figure(figsize=(12, 9))
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    VisualizePoints(P_dlg, color='red', label='dlg_points')
+    VisualizePoints(P_dopp, color='green', label='dopp_points')
+    VisualizeMacthLine(P_dlg, P_dopp, row, col)
+    plt.axis('equal')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Match_' + pic)
+    plt.legend(loc='best')
+    plt.savefig('..\\pic\\Match_' + pic + '.png', dpi=300)
+    plt.close()
---
+++ GraphMatchByLineIntersectionPoints/src/all_MaxPoints.py
@@ -0,0 +1,79 @@
+# File      :all.py
+# Author    :WJ
+# Function  :
+# Time      :2021/02/21
+# Version   :
+# Amend     :
+
+import LineProcess as lp
+import numpy as np
+import LaplacianMatrice as lm
+from scipy.optimize import linear_sum_assignment
+import TransformationMatrix as tf
+import Visualization as vs
+import LineFit_MaxPoints as lf
+import time
+
+start0 = time.time()
+# 导入数据
+data_dlg = np.loadtxt('..\\data\\Polyline_PCB02_500.txt', delimiter=',')
+data_dopp = np.loadtxt('..\\data\\PCB_c0.5_z1_t10.txt', delimiter='\t')
+data_dlg0 = data_dlg[:, 0:2]
+data_dopp0 = data_dopp[:, 0:2]
+
+# 合并重叠点及邻近点
+data_dlg = lf.mergeClosePoints(data_dlg0)
+data_dopp = lf.mergeClosePoints(data_dopp0, 1.8)
+
+# 搜索直线
+Lines_dlg = lf.searchLines(data_dlg, 16, 6, 0.5)
+Lines_dopp = lf.searchLines(data_dopp, 15, 6, 0.5)
+
+# 处理直线
+Lines_dlg = lf.processLines(Lines_dlg)
+Lines_dlg = lf.mergeLines_ABC(Lines_dlg, X0=np.mean(data_dlg[:, 0]), Y0=np.mean(data_dlg[:, 1]), slope=0.1,
+                              intercept=12)
+Lines_dopp = lf.processLines(Lines_dopp)
+Lines_dopp = lf.mergeLines_ABC(Lines_dopp, X0=np.mean(data_dopp[:, 0]), Y0=np.mean(data_dopp[:, 1]), slope=0.1,
+                               intercept=12)
+
+# 可视化直线
+lf.showLines(data_dlg, Lines_dlg, np.mean(data_dlg[:, 0]), np.mean(data_dlg[:, 1]),
+             name='Lines_dlg' + str(len(Lines_dlg)))
+lf.showLines(data_dopp, Lines_dopp, np.mean(data_dopp[:, 0]), np.mean(data_dopp[:, 1]),
+             name='Lines_dopp' + str(len(Lines_dopp)))
+
+P_dopp = lp.GetIntersectPointofLines(Lines_dopp)
+P_dlg = lp.GetIntersectPointofLines(Lines_dlg)
+
+L_dopp = lm.LaplacianMatrice(P_dopp, sigma=500)
+L_dlg = lm.LaplacianMatrice(P_dlg, sigma=500)
+
+U_dopp, Lambda_dopp = lm.LaplacianMatrice_decomposed(L_dopp)
+U_dlg, Lambda_dlg = lm.LaplacianMatrice_decomposed(L_dlg)
+
+# 计算相异度矩阵
+k = min(len(P_dlg), len(P_dopp))
+A = lm.corrlation(U_dopp, U_dlg, k)
+
+# 对相似度矩阵进行二分匹配(删除相异度过大的结果)
+row_ind, col_ind = linear_sum_assignment(A)
+row, col = lm.DeleteLargeValue(A, row_ind, col_ind, 0.9)
+
+# 根据匹配结果对点云重新排序
+P_dlg_new=lm.resort_clouds(P_dlg,row)
+P_dopp_new=lm.resort_clouds(P_dopp,col)
+
+# 可视化直线交点匹配结果
+vs.VisualizeMatch(P_dopp, P_dlg, row, col,'直线交点')
+
+# 计算变换矩阵(并对dopp进行变换)
+R, T = tf.ca_rt(P_dopp_new, P_dlg_new, 'MatchingByConPolyPoints_result.txt')
+data_dopp = tf.transformation(data_dopp0, R, T, 'dopp_transformed.txt')
+
+# 可视化原始点云配准结果
+vs.Visualize2PointClouds(data_dopp, data_dlg0, 'Macth_dlg&dopp', feature1=['blue', 'dopp', '.'],
+                         feature2=['red', 'dlg', '.'])
+
+TIME = time.time() - start0
+print('\n总耗时:{:.0f} hours {:.0f} minutes {:.0f} seconds'.format(TIME // 3600, TIME % 3600 // 60, TIME % 3600 % 60))
